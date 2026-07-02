81a786b40ea11504f34b8e2f860a49cc8c8cfec3
nguyenhoanghuy34 <ng.hhuy03@gmail.com>
2026-07-02 20:20:39 +0700

    Glassmorphism

--- src/features/auth/pages/LoginPage.jsx
+++ src/features/auth/pages/LoginPage.jsx
@@ -1,14 +1,33 @@
 import AuthForm from "../components/AuthForm";
+import "../auth.css";
+
+import loginBackground from "../../../assets/login_background.jpg";
 
 export default function LoginPage() {
-  return (
-    <div className="auth-container">
-      <div className="auth-card">
-        <h2 className="title">Welcome back</h2>
-        <p className="subtitle">Login to continue</p>
+    return (
+        <div
+            className="login-page"
+            style={{
+                backgroundImage: `url(${loginBackground})`,
+            }}
+        >
+            <div className="login-overlay">
 
-        <AuthForm />
-      </div>
-    </div>
-  );
+                <div className="glass-card">
+
+                    <h1 className="title">
+                        Welcome Back
+                    </h1>
+
+                    <p className="subtitle">
+                        Sign in to continue
+                    </p>
+
+                    <AuthForm />
+
+                </div>
+
+            </div>
+        </div>
+    );
 }
